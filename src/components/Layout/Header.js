958dcc502d279654eaf973a87332b30087ac4aa5
SammyTBrains <josephsamuel034@gmail.com>
2023-02-20 19:02:15 +0100

    Header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Test Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Table full of delicious food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
